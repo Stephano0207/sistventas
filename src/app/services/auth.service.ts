e84d97d03f9f4db4ab1bd5f43931179c3fb9b5b7
Stephano0207 <castanedastephano16@gmail.com>
2024-10-03 15:16:19 -0500

    Tarea_semana_03

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  url:string="http://localhost:8000/api/login";
+  constructor(private http:HttpClient) { }
+  public verificarEmail(email:string){
+    return this.http.get<any>(this.url+`/`+email);
+  }
+  public verificarClave(email:string,clave:string){
+    return this.http.get<any>(this.url+`/`+email+`/`+clave);
+  }
+}
